5196599351e24b0c3c0dcd742c1aba58f097f371
DocXydre <totolego88@gmail.com>
2020-11-24 11:53:32 +0100

    les couleurs ready

--- couleur_aleatoire.py
+++ couleur_aleatoire.py
@@ -6,4 +6,7 @@ def couleur_aleatoire():
     renvoie un triplet de 3 nombres entier compris entre 0 et 255
     Ce triplet correspond à une couleur codée en RVB
     '''
-    pass
+    Rouge = randint(0,255)
+    Vert = randint(0,255)
+    Bleu = randint(0,255)
+    return (Rouge,Vert,Bleu)
